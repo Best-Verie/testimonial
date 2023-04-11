ebec18e01322d9f5ec3e6512d20b32ab632cee9b
Best-Verie <irabestverie2016@gmail.com>
2023-04-11 11:22:50 +0200

    feat: adds responsible flexbox to the main screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,28 @@
 export default function Index() {
-  return <div>Hello World</div>;
+  return (
+    <div className="flex h-screen ">
+      <div
+        className="flex justify-center items-center w-30 bg-gray-300 p-4"
+        style={{ flex: "0 0 30%" }}
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In non,
+          consequuntur similique nihil ex amet eius minus pariatur beatae, rerum
+          unde repellat, delectus nostrum blanditiis? Perspiciatis cum voluptas
+          est ratione?
+        </p>
+      </div>
+      <div
+        className="flex justify-center items-center w-70 bg-gray-500 p-4"
+        style={{ flex: "0 0 70%" }}
+      >
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi
+          quisquam ratione pariatur perspiciatis numquam quidem porro iste
+          dolores veritatis distinctio, fuga veniam sequi, officia sunt
+          voluptatum eligendi dolore asperiores ad.
+        </p>
+      </div>
+    </div>
+  );
 }
